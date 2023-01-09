d814f65c77a9e4499e7d7e296028a2dcc2d32bcd
Long Phan <111166256+longphanquangminh@users.noreply.github.com>
2023-01-09 13:28:26 +0700

    thietLapKhacTab

--- page/deleteBusiness/index.js
+++ page/deleteBusiness/index.js
@@ -8,7 +8,7 @@ module.exports = {
     deleteBusiness(tenDN) {
         I.waitForElement(homePageLocator.menu.settingTab);
         customMethod.clickElement(homePageLocator.menu.settingTab);
-        I.waitForElement(homePageLocator.menu.settingTab,timeout.loading_element);
+        I.waitForElement(homePageLocator.menu.thietLapKhacTab, timeout.loading_element);
         customMethod.clickElement(homePageLocator.menu.thietLapKhacTab);
         I.waitForElement(homePageLocator.home.deleteBusinessDropdown, timeout.loading_element)
         I.click(homePageLocator.home.deleteBusinessDropdown)
